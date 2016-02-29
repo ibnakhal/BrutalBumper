0d24696fe2f3c9631c2f2e8d067ecabb2b6e47cc
ibnakhal <inakhal2@180-705-16.students.aac.academyart.edu>
2016-02-28 22:42:36 -0800

    Health Script and Vehicle control

---
+++ Assets/Scripts/Player/Bumper.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bumper : MonoBehaviour {
+    [SerializeField]
+    private int damage;
+    [SerializeField]
+    private GameObject parent; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    public void OnTriggerEnter(Collider Other)
+    {
+        if (Other.tag == "Player" && Other.gameObject != parent)
+        {
+            Other.GetComponent<HealthScriptNewBehaviourScript>().GetHurt(damage);
+        }
+    }
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Player/VehicleControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleControl : MonoBehaviour {
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float turnspeed;
+    [SerializeField]
+    private Rigidbody rb;
+    // Use this for initialization
+    void Start () {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Mathf.Abs(Input.GetAxis("Vertical")) > 0.01f)
+        {
+            Vector3 thrustV = transform.forward;
+            thrustV.y = 0.0f;
+            rb.AddRelativeForce(thrustV * speed);
+            //transform.Translate(transform.forward * speed * Time.deltaTime);
+        }
+	
+	}
+}
