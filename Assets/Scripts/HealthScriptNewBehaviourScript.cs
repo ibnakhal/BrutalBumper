a75f0daf397765b75eaf0813a8cb82e8840361ff
ibnakhal <inakhal2@180-705-16.students.aac.academyart.edu>
2016-02-28 18:35:56 -0800

    Health System Created

---
+++ Assets/Scripts/HealthScriptNewBehaviourScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScriptNewBehaviourScript : MonoBehaviour {
+    [SerializeField]
+    private int Health;
+    [SerializeField]
+    private ParticleSystem deathParticles;
+    [SerializeField]
+    private GameObject deadMesh;
+     
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(Health<=0)
+        {/*
+            Instantiate(deathParticles, transform.position, Quaternion.identity);
+            Instantiate(deadMesh, transform.position, transform.rotation);
+            
+            */
+            Destroy(this);
+        }
+	}
+
+
+
+    public void GetHurt(int damage)
+    {
+        Health -= damage;
+    }
+}
