1006cabad7d2757029eb41309fd371526d8f2312
ibnakhal <inakhal2@180-705-16.students.aac.academyart.edu>
2016-02-28 17:48:41 -0800

    main scene created, testing out control framework

---
+++ Assets/Scripts/VehicleControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleControl : MonoBehaviour {
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float turnspeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Mathf.Abs(Input.GetAxis("Horizontal")) > 0.01f)
+        {
+            transform.Translate(transform.forward * speed * Time.deltaTime);
+        }
+	
+	}
+}
