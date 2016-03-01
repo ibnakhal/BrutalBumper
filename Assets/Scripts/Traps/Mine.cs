e1ec70810ef4daa63fa6bd672ebe9b2d4e94da60
Leprechauno <tyler.missel@gmail.com>
2016-02-29 23:29:47 -0800

    started mine script
    trying to give the mine a blast radius

---
+++ Assets/Scripts/Traps/Mine.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mine : MonoBehaviour
+{
+    [SerializeField]
+    private int damage = 10;
+    [SerializeField]
+    private float damageRadius = 5;
+    private HealthScriptNewBehaviourScript health = null;
+
+    private void MineDamage(Vector3 center, float radius)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(center, radius);
+        int i = 0;
+        while(i < hitColliders.Length)
+        {
+            hitColliders[i].GetComponent<HealthScriptNewBehaviourScript>().GetHurt(damage);
+            //health.GetHurt(damage);
+            i++;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            //MineDamage(this.transform.position, damageRadius);
+            Destroy(this.gameObject);
+            print("exploding");
+        }
+    }
+}
