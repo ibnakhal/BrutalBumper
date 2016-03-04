3424e6df556a6b285b20db96184e6eaae4c551cc
ibnakhal <INAKHAL2@art.edu>
2016-03-03 21:00:25 -0800

    main menu created

---
+++ Assets/Scripts/UI/Button.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class Button : MonoBehaviour {
+    [SerializeField]
+    private int arcadeLevel;
+    [SerializeField]
+    private int splitLevel;
+    [SerializeField]
+    private GameObject confirmPanel;
+    [SerializeField]
+    private GameObject confirmButton1;
+    [SerializeField]
+    private GameObject confirmButton2;
+    public void Arcade()
+    {
+//        Application.LoadLevel(arcadeLevel);
+        SceneManager.LoadScene(arcadeLevel);
+    }
+    public void Confirm()
+    {
+        confirmPanel.SetActive(true);
+        confirmButton1.SetActive(true);
+        confirmButton2.SetActive(true);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void SplitScreen()
+    {
+        SceneManager.LoadScene(splitLevel);
+
+    }
+    public void Denied()
+    {
+        confirmPanel.SetActive(false);
+        confirmButton1.SetActive(false);
+        confirmButton2.SetActive(false);
+    }
+}
